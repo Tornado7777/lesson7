7bcfeb6922573c57062efb97fda1d7e6a8cab843
Ivan <tornadod@mail.ru>
2021-05-16 21:06:43 +0300

    lesson7 part 2 ready

---
+++ lesson7.2/lesson7.1/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("lesson7.1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("lesson7.1")]
+[assembly: AssemblyTitle("lesson7.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ lesson7.2/lesson7.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: lesson7._1.Program
+// Assembly: lesson7.1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 7D91161B-341C-4059-9008-2557856C210E
+// Assembly location: C:\Users\Locky\source\repos\lesson7\lesson7.1\bin\Debug\net5.0\lesson7.1.dll
+
+using System;
+
+namespace lesson7._1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Введите своё имя:");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine(" Привет, " + Console.ReadLine() + "! Для выхода из программы нажми любую клавишу!");
+      Console.ReadLine();
+    }
+  }
+}
