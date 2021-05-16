22bef701ba3087930690948c12ce664149959106
Ivan <tornadod@mail.ru>
2021-05-16 20:34:04 +0300

    lesson7 part 1 ready

---
+++ lesson7.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lesson7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Введите своё имя:");
+            name = Console.ReadLine();
+            Console.WriteLine($" Привет, { name }! Для выхода из программы нажми любую клавишу!");
+            Console.ReadLine();
+        }
+    }
+}
